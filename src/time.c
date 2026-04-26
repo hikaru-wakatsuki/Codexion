c505b0ba9661a221b1c960babaf141e971c0556e
Hikaru Wakatsuki <hwakatsu@c5r3s13.42tokyo.jp>
2026-04-26 19:13:53 +0900

    waka

--- src/time.c
+++ src/time.c
@@ -1,22 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   time.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/24 19:52:49 by hwakatsu          #+#    #+#             */
-/*   Updated: 2026/04/26 18:59:34 by hwakatsu         ###   ########.fr       */
+/*   Created: 2026/04/26 19:01:35 by hwakatsu          #+#    #+#             */
+/*   Updated: 2026/04/26 19:13:24 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
 
-int main(int argc, char *argv[])
+long	get_time_ms(void)
 {
-	if (argc != 9)
-		return (0);
+	struct timeval	tv;
 
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000L + tv.tv_usec / 1000L);
 }
 
+long	timestamp_ms(t_sim *sim)
+{
+	return (get_time_ms() - sim->start_ms);
+}
 
