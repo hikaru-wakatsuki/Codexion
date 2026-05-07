d28ed7af32410f7c63aefaa4e856f0c42bd79bc7
hikaru wakatsuki <hikaru@student.42tokyo.jp>
2026-05-07 02:38:18 +0000

    wak

--- src/dongles_take.c
+++ src/dongles_take.c
@@ -1,25 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   dongles.c                                          :+:      :+:    :+:   */
+/*   dongles_take.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/30 12:47:07 by hwakatsu          #+#    #+#             */
-/*   Updated: 2026/05/06 14:18:13 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2026/05/07 02:37:06 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/codexion.h"
 
-static bool	is_my_turn(t_dongle *dongle, int coder_id)
+static bool	can_take_dongle(t_coder *coder, t_dongle *dongle)
 {
 	t_request	top;
 
 	if (dongle->wait_queue.size == 0)
 		return (false);
 	top = dongle->wait_queue.data[0];
-	return (top.coder_id == coder_id);
+	return (top.coder_id == coder->id
+		&& dongle->owner_coder_id != -1
+		&& get_time_ms() >= dongle->cooldown_until_ms);
+}
+
+static void	ms_to_timespec(long ms, struct timespec *ts)
+{
+	ts->tv_sec = ms / 1000;
+	ts->tv_nsec = (ms % 1000) * 1000000;
+}
+
+static bool	wait_for_turn(t_coder *coder, t_dongle *dongle)
+{
+	struct timespec	ts;
+
+	while (!can_take_dongle(coder, dongle))
+	{
+		if (is_stopped(coder->sim))
+		{
+			pthread_mutex_unlock(&dongle->mutex);
+			return (false);
+		}
+		if (dongle->wait_queue.size > 0
+			&& dongle->wait_queue.data[0].coder_id == coder->id
+			&& dongle->owner_coder_id != -1)
+		{
+			ms_to_timespec(dongle->cooldown_until_ms, &ts);
+			pthread_cond_timedwait(&dongle->cond, &dongle->mutex, &ts);
+		}
+		else
+			pthread_cond_wait(&dongle->cond, &dongle->mutex);
+	}
+	return (true);
 }
 
 static void	take_dongle(t_coder *coder, t_dongle *dongle)
@@ -33,17 +65,8 @@ static void	take_dongle(t_coder *coder, t_dongle *dongle)
 	pthread_mutex_unlock(&coder->sim->seq_mutex);
 	pthread_mutex_lock(&dongle->mutex);
 	push_request(&dongle->wait_queue, req, coder->sim);
-	while (!is_my_turn(dongle, coder->id)
-		|| dongle->owner_coder_id != -1
-		|| get_time_ms() < dongle->cooldown_until_ms)
-	{
-		if (is_stopped(coder->sim))
-		{
-			pthread_mutex_unlock(&dongle->mutex);
-			return ;
-		}
-		pthread_cond_wait(&dongle->cond, &dongle->mutex);
-	}
+	if (!wait_for_turn(coder, dongle))
+		return ;
 	pop_request(&dongle->wait_queue, coder->sim);
 	dongle->owner_coder_id = coder->id;
 	pthread_mutex_unlock(&dongle->mutex);
@@ -67,20 +90,4 @@ void	take_dongles(t_coder *coder)
 	}
 	take_dongle(coder, &coder->sim->dongles[first]);
 	take_dongle(coder, &coder->sim->dongles[second]);
-}
-
-static void	release_dongle(t_coder *coder, t_dongle *dongle)
-{
-	pthread_mutex_lock(&dongle->mutex);
-	dongle->owner_coder_id = -1;
-	dongle->cooldown_until_ms = get_time_ms() + coder->sim->dongle_cooldown;
-	pthread_cond_broadcast(&dongle->cond);
-	pthread_mutex_unlock(&dongle->mutex);
-	print_log(coder->sim, coder->id, "released a dongle");
-}
-
-void	release_dongles(t_coder *coder)
-{
-	release_dongle(coder, &coder->sim->dongles[coder->left_dongle_idx]);
-	release_dongle(coder, &coder->sim->dongles[coder->right_dongle_idx]);
 }
